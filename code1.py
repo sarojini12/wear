56295fa883b74b12a3f5cb909074b36d56e04b16
sarojini12 <36305202+sarojini12@users.noreply.github.com>
2018-02-09 22:18:52 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,7 +1,7 @@
 num=int(input("enter the number"))
-if(num==0)
-print=0
-if(num>=0)
-print=positive
-if(num<=0)
-print(negative)
+if(num==0):
+ print ("0")
+if(num>=0):
+ print("positive")
+ if(num<=0):
+print("negative")
